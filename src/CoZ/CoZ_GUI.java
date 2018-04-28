785df4946f548a0e438131a4af1e0737f70f9ebc
RomanRozin2m2 <romanrozin2m2@yandex.ru>
2018-04-28 19:42:26 +0300

    oppa

--- src/CoZ/CoZ_GUI.java
+++ src/CoZ/CoZ_GUI.java
@@ -64,6 +64,8 @@ public class CoZ_GUI extends JFrame{
 
     }
 
+    private class veryClassNo {}//extends shelf
+
     private int fieldSize;
     private JTextArea field;
     private Line[] verticalLines;
